3ff7dbd5b6c4cba3aeccb61df373898e64ba9880
Iñaki Garro <ignakigarro@gmail.com>
2024-03-16 13:13:54 -0300

    chore: refactor query placeholder method

--- dotnet/Project-D.Api/GraphQl/Query.cs
+++ dotnet/Project-D.Api/GraphQl/Query.cs
@@ -2,8 +2,5 @@ namespace Project_D.Api.GraphQl;
 
 public class Query
 {
-    public string Placeholder()
-    {
-        return "This query is a placeholder.";
-    }
+    public string Placeholder() => "This query is a placeholder.";
 }
