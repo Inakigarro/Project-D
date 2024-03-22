f69354d4bb86d73716cb7485ec0fb5f5678db875
Iñaki Garro <ignakigarro@gmail.com>
2024-03-22 18:27:31 -0300

    feat: form builder

---
+++ angular/src/components/forms/form-builder/form-builder.ts
@@ -0,0 +1,34 @@
+import { Injectable } from "@angular/core";
+import { FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
+
+export interface DynamicFormField<TItem> {
+    name: string & keyof TItem;
+    type: string;
+    defaultValue: unknown,
+    validators: Validators
+}
+
+@Injectable({providedIn: 'root'})
+export class DynamicFormBuilder<TItem extends {}> {
+    public formFields: DynamicFormField<TItem>[] = []
+
+    public formGroup = this.formBuilder.group({});
+
+    constructor(private formBuilder: FormBuilder){}
+
+    public addFormField(field: DynamicFormField<TItem>) {
+        this.formFields.push(field);
+        return this;
+    }
+
+    public buildForm() {
+        this.formFields.forEach(field => {
+            let control = this.formBuilder.control({
+                disabled: false,
+                value: field.defaultValue
+            },field.validators)
+            this.formGroup.addControl(field.name, control);
+        });
+        return this.formGroup;
+    }
+}
